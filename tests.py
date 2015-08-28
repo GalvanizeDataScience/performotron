2d65858a2155f1ed3fa76d2b81fa71381847faca
Isaac Laughlin <isaac.laughlin@galvanize.com>
2015-08-28 10:22:30 -0700

    Fixing up tests to reflect changes.

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from model import Comparer
+from performotron import Comparer
 from sklearn import datasets, cross_validation, linear_model
 from nose.tools import assert_equal
 
@@ -9,28 +9,27 @@ def get_comparer():
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(data, target,
                                                             test_size=.2)
 
-    OLS = linear_model.LinearRegression()
-    OLS.fit(X_train, y_train)
+
+    #OLS = linear_model.LinearRegression()
+    #OLS.fit(X_train, y_train)
 
     url = 'https://hooks.slack.com/services/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'
-    c = Comparer(OLS, X_test, y_test, url)
+    
+    c = Comparer(y_test)
     return c, X_test, y_test
 
 
-def test_prediction():
-    c , _, y_test = get_comparer()
-    assert_equal(len(c.prediction), len(y_test))
-
 def test_score():
-    c, _, _ = get_comparer()
-    assert c.score()
-    assert c.score() > 0
+    c, _, y_test = get_comparer()
+    #assert c.score(y_test)
+    #Score is zero cause we're scoring test data.
+    assert c.score(y_test) == 0
 
 
-def test_slack():
-    c, _, _ = get_comparer()
+#def test_slack():
+ #   c, _, y_test = get_comparer()
 
-    resp = c.report_to_slack("#debug", "isaac.laughlin")
-    assert_equal(resp.status_code, 200)
+    #resp = c.report_to_slack(y_test)
+    #assert_equal(resp.status_code, 200)
     
 
